63203358c7472ef20012d01d07621428fd572e46
DESKTOP-BGP888H\ldj <lbj@cudo.co.kr>
2021-04-23 10:22:41 +0900

    필요없는 주석 모두 삭제

--- cudomediasite-main/src/main/java/com/cudo/mediabusiness/mediasite/MediaSiteApplication.java
+++ cudomediasite-main/src/main/java/com/cudo/mediabusiness/mediasite/MediaSiteApplication.java
@@ -2,10 +2,7 @@ package com.cudo.mediabusiness.mediasite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.sql.DataSource;
 
 @EnableJpaAuditing
 @SpringBootApplication
